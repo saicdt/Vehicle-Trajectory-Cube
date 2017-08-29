880f748d93ba20ec5edf10cf5d7ed5272f51ea33
Administrator <Administrator@windows10.microdone.cn>
2017-08-29 14:44:41 +0800

    v001 remove the dup

--- 3.2.1/Nanocubes_refractor/src/NanoCubeQueryException.cc
+++
@@ -1,9 +0,0 @@
-#include "NanoCubeQueryException.hh"
-
-//------------------------------------------------------------------------------
-// QueryException Impl.
-//------------------------------------------------------------------------------
-
-nanocube::query::QueryException::QueryException(const std::string &message):
-    std::runtime_error(message)
-{}
--- 3.2.1/Nanocubes_refractor/src/geom2d/geom2d.hh
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "base.hh"
-#include "point.hh"
-#include "polygon.hh"
-#include "tile.hh"
-#include "make_monotone.hh"
-#include "planegraph.hh"
